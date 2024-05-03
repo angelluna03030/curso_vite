c04a63b61f371bda63a51c77c5ef1f36380362ba
angel luna <angelstivengarcialuna@gmail.com>
2024-05-02 22:57:16 -0500

    creando un hooks,en la parte de useSearchGifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-import React, { useState } from "react";
+
 import ContenidoImagen from "./ContenidoImagen";
-
+import {useSearchGifs } from "./useSearchGifs"
+import "./app.css"
 const App = () => {
-  const [query, setQuery] = useState(""); // Estado para almacenar el término de búsqueda
-  const [gifs, setGifs] = useState([]); // Estado para almacenar los GIFs
-  const apiKey = "CvFChQwhxqO86996HEv1p66cTle7CYUv";
-  const baseUrl = "https://api.giphy.com/v1/gifs/search";
-
-  // Función para realizar la solicitud de búsqueda de GIFs
-  const getGifs = async () => {
-    try {
-      const url = `${baseUrl}?api_key=${apiKey}&q=${query}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setGifs(data.data); // Actualizar el estado de los GIFs con los resultados de la búsqueda
-    } catch (error) {
-      console.error("Error fetching GIFs:", error);
-    }
-  };
-
-  // Manejar el envío del formulario
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    getGifs(); // Llamar a la función para buscar GIFs
-  };
-
+ const {handleSubmit ,query, gifs, setQuery } = useSearchGifs()
   return (
     <>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)} // Actualizar el estado del término de búsqueda
+         value={query}
+          onChange={(e) => setQuery(e.target.value)} 
           placeholder="Buscar GIFs..."
         />
         <button type="submit">Buscar</button>
---
+++ src/useSearchGifs.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export const useSearchGifs = () => {
+  const [query, setQuery] = useState(""); // Estado para almacenar el término de búsqueda
+  const [gifs, setGifs] = useState([]); // Estado para almacenar los GIFs
+  const apiKey = "CvFChQwhxqO86996HEv1p66cTle7CYUv";
+  const baseUrl = "https://api.giphy.com/v1/gifs/search";
+
+  // Función para realizar la solicitud de búsqueda de GIFs
+  const getGifs = async () => {
+    try {
+      const url = `${baseUrl}?api_key=${apiKey}&q=${query}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setGifs(data.data); // Actualizar el estado de los GIFs con los resultados de la búsqueda
+    } catch (error) {
+      console.error("Error fetching GIFs:", error);
+    }
+  };
+
+  // Manejar el envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getGifs(); // Llamar a la función para buscar GIFs
+  };
+
+  return {
+    handleSubmit,
+    setQuery, // Devolver la función para actualizar el estado del término de búsqueda
+    query,
+    gifs,
+  };
+};
